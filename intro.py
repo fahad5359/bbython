1be81f54995a5f4c699b587640917abea29558ea
fahad5359 <fabbgm5359@gmail.com>
2022-02-05 22:09:28 +0300

    sandbox

--- intro.py
+++ intro.py
@@ -1,3 +1,11 @@
-print("hey")
-print("go"+"asdff")
-print("sfde")
+print("hey");
+print("go"+"asdff");
+print("sfde");
+cars = ["Ford", "Volvo", "BMW"]
+anmails = ["dog","cat"]
+# asd =[{"name":"fahad"},
+#       {"nomm":"ali"},
+#       {"klb":"cat"}]
+# print(asd.n)
+# print(cars +anmails);
+print(cars[1])
