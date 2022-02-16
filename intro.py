8edc2607f1607069c54364f75e936b7643360230
fahad5359 <fabbgm5359@gmail.com>
2022-02-16 19:51:35 +0300

    varibles

--- intro.py
+++ intro.py
@@ -1,11 +1,17 @@
-print("hey");
-print("go"+"asdff");
-print("sfde");
-cars = ["Ford", "Volvo", "BMW"]
-anmails = ["dog","cat"]
-# asd =[{"name":"fahad"},
-#       {"nomm":"ali"},
-#       {"klb":"cat"}]
-# print(asd.n)
-# print(cars +anmails);
-print(cars[1])
+# print("hey");
+# print("go"+"asdff");
+# print("sfde");
+# cars = ["Ford", "Volvo", "BMW"]
+# anmails = ["dog","cat"]
+# # asd =[{"name":"fahad"},
+# #       {"nomm":"ali"},
+# #       {"klb":"cat"}]
+# # print(asd.n)
+# # print(cars +anmails);
+# print(cars[1])
+
+# ////////////////////////////////////////////// section 1 varibles in python ////////////////////////////////////////////
+
+nem = "fahad";
+age = 50;
+print(nem,age)
